ad84076197995892528f6d2a2c7763841672aa8a
Shivang Singh <57583381+Shivang2003@users.noreply.github.com>
2026-04-20 04:47:07 +0530

    07:23:35

--- students-api/cmd/students-api/main.go
+++ students-api/cmd/students-api/main.go
@@ -1,8 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/shivang2003/students-api/internal/config"
+)
 
 func main() {
 
-	fmt.Println("Hello, Students API!")
+	//load the config
+	cfg := config.MustLoad()
+
+	//db setup
+
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to students api"))
+	})
+
+	//setup server
+	server := http.Server{
+		Addr:     cfg.HTTPServer.Addr,
+		Handler: router,
+	}
+	fmt.Printf("Server started at %s", cfg.HTTPServer.Addr)
+	err := server.ListenAndServe()
+	if err != nil{
+		log.Fatal("failed to start server")
+	}
+
+	
 }
